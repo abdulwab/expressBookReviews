4118a0052d647a1aa1a46041552982bd0bb4cc31
AbdulWahabAwan <alvilahori@gmail.com>
2024-06-28 16:55:35 +0500

    complete the customer auth route

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,42 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provider." });
+  }
+  try {
+    const decode = jwt.verify(token, "Enter Secret key");
+    req.user = decode;
+    next();
+  } catch (ex) {
+    res.status(400).json({ message: "Invalid token..." });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
